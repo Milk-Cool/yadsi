816f8d0e9482c931ae6a3fd74101ca2cadc6c48c
Milk-Cool <coder.tomsk@gmail.com>
2025-08-09 13:44:22 +0300

    basic storage

---
+++ src/files.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define CONFIG_NAME "/yadsi.conf"
+#define CONFIG_CONTENTS "# Filename of the script to execute, including \".txt\" and excluding \"scripts/\".\n\
+# Example: name=helloworld_win.txt\n\
+name="
+
+#define SCRIPTS_NAME "/scripts"
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,50 @@
 #include <Arduino.h>
+#include <Keyboard.h>
+#include <FatFS.h>
+#include <FatFSUSB.h>
+
+#include "files.h"
+
+bool is_executing = false;
+bool mountable = true;
+bool usb_mounted = false;
+
+void on_plug(uint32_t) {
+    FatFS.end();
+    usb_mounted = true;
+}
+void on_unplug(uint32_t) {
+    FatFS.begin();
+    usb_mounted = false;
+}
+
+void init_files() {
+    if(!FatFS.exists(CONFIG_NAME)) {
+        File f = FatFS.open(CONFIG_NAME, "w");
+        f.write(CONFIG_CONTENTS);
+        f.close();
+    }
+    if(!FatFS.exists(SCRIPTS_NAME))
+        FatFS.mkdir(SCRIPTS_NAME);
+}
 
 void setup() {
     Serial.begin(115200);
+    FatFS.begin();
+    init_files();
+    FatFSUSB.onPlug(on_plug);
+    FatFSUSB.onUnplug(on_unplug);
+    FatFSUSB.begin();
 }
 
 void loop() {
-    if(BOOTSEL) Serial.println("BOOTSEL!!!");
-    delay(200);
+    if(!is_executing && BOOTSEL) {
+        is_executing = true;
+        if(mountable) {
+            FatFSUSB.unplug();
+            FatFSUSB.end();
+            mountable = false;
+        }
+    }
+    delay(5);
 }
